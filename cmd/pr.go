44b970d43c6d9a422e7230da136b42bbe621fac4
Taj <tjgurwara99@gmail.com>
2022-03-27 00:28:14 +0000

    fix: removed unnecessary comments

--- cmd/pr.go
+++ cmd/pr.go
@@ -56,14 +56,4 @@ var prsCmd = &cobra.Command{
 
 func init() {
 	listCmd.AddCommand(prsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// prsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// prsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/root.go
+++ cmd/root.go
@@ -36,8 +36,6 @@ var rootCmd = &cobra.Command{
 	Use:   "ghcli",
 	Short: "Simple github cli tool",
 	Long:  `Simple github cli tool`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -48,16 +46,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ghcli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
